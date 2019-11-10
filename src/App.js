ca86f75cc18d2ca1fa98ec3e3a8b5ea0706dbb67
Petryxan <petryxan@seznam.cz>
2019-11-10 08:28:55 +0100

    add folder src

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TodoList from './Todo/TodoList'
+
+
+function App () {
+  return (
+    <div className="wrapper">
+      <h1>React tutorial</h1>
+      <h2>test223332</h2>
+    <TodoList />
+
+    </div>
+  )
+  
+}
+
+export default App;
+
